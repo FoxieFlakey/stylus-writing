afba2477fa0710ebc990cc2e2ec05a993d6f1208
Foxie Flakey <foxieflakey@gmail.com>
2025-12-14 23:06:47 +0700

    Keep track number of updates

--- src/writing_canvas.rs
+++ src/writing_canvas.rs
@@ -1,11 +1,12 @@
 use std::{cell::RefCell, rc::Rc};
 
-use sdl3::{pixels::Color, render::Canvas, video::Window};
+use sdl3::{pixels::{Color, PixelFormat}, render::Canvas, video::Window};
 
 use crate::shapes::{Rect, Stroke, Point};
 
 pub struct WritingCanvas {
   bound: Rect,
+  update_count: u64,
   stroke_distance_threshold: f32,
   // .0 = which pen
   // .1 = whether the pen is out or in the bound
@@ -19,15 +20,36 @@ impl WritingCanvas {
     Self {
       bound,
       canvas,
+      update_count: 0,
       current_pen: None,
       stroke_distance_threshold: 3.0,
       all_strokes: Vec::new()
     }
   }
   
+  pub fn with_pixels<R, F: FnOnce(&[u8], u32, u32, u32, PixelFormat) -> R>(&self, func: F) -> R {
+    let canvas = self.canvas.borrow();
+    let surface = canvas
+      .read_pixels(Some(self.bound.clone().into()))
+      .unwrap();
+    
+    let width = surface.width();
+    let height = surface.height();
+    let pitch = surface.pitch();
+    let pixel_format = surface.pixel_format();
+    
+    surface.with_lock(|bytes| {
+      func(bytes, width, height, pitch, pixel_format)
+    })
+  }
+  
   pub fn get_stroke_count(&self) -> usize {
     self.all_strokes.len()
   } 
+  
+  pub fn get_update_count(&self) -> u64 {
+    self.update_count
+  }
   
   pub fn pen_down(&mut self, x: f32, y: f32, pen: u32) {
     if !self.bound.contains(&Point { x, y }) {
@@ -46,6 +68,7 @@ impl WritingCanvas {
       start: Point { x, y },
       end: Point { x, y }
     });
+    self.update_count += 1;
   }
   
   pub fn pen_up(&mut self, x: f32, y: f32, pen: u32) {
@@ -61,6 +84,7 @@ impl WritingCanvas {
     }
     
     self.all_strokes.last_mut().unwrap().end = Point { x, y };
+    self.update_count += 1;
   }
   
   pub fn pen_motion(&mut self, x: f32, y: f32, pen: u32) {
@@ -90,6 +114,8 @@ impl WritingCanvas {
     
     let latest_stroke = self.all_strokes.last_mut().unwrap();
     latest_stroke.end = Point { x, y };
+    self.update_count += 1;
+    
     if latest_stroke.length() >= self.stroke_distance_threshold {
       self.all_strokes.push(Stroke {
         start: Point { x, y },
