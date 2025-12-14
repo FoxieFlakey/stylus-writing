40101a8d51af70993b9965268ca67eb93b7a8012
Foxie Flakey <foxieflakey@gmail.com>
2025-12-15 00:20:52 +0700

    Finally recognize text

--- src/processing_thread.rs
+++ src/processing_thread.rs
@@ -25,21 +25,16 @@ pub fn main() {
       continue;
     }
     latest_update_id = update_id;
-    
-    log::info!("There new update");
-    image_data.clear();
-    
-    thread::sleep(Duration::from_secs(1));
-    
-    let details = pixels.format.details();
-    log::info!("Pixel format info {:#?}", details);
     assert!(matches!(pixels.format, PixelFormat::RGB24));
     
     let rgb_image = ImageBuffer::<Rgb<u8>, &[u8]>::from_raw(u32::try_from(pixels.width).unwrap(), u32::try_from(pixels.height).unwrap(), &pixels.data).unwrap();
+    image_data.clear();
     rgb_image.write_with_encoder(PngEncoder::new(&mut image_data)).unwrap();
     
-    let mut file = File::create(format!("/tmp/screeenshot-{update_id}.png")).unwrap();
-    file.write_all(&image_data).unwrap();
+    api.set_image_from_mem(&image_data).unwrap();
+    
+    let recognized = api.get_utf8_text().unwrap();
+    log::info!("Text recognized: {}", recognized.trim());
   }
   
   log::info!("Processing thread stapped");
